f848719e05369792a8cc1a3cdcea1635283b0fee
Marton <martin.iribass@gmail.com>
2024-06-03 20:05:22 -0300

    fix: deploy

--- src/pages/admin/TablerUsers.tsx
+++ src/pages/admin/TablerUsers.tsx
@@ -1,10 +1,8 @@
-import NotPass from "@/components/errorPage/NotPass";
 import { TablerUserShad } from "@/components/tabs/TablerUserShad";
-import useAuthStore from "@/services/authState";
 import React from "react";
 
 const TablerUsers: React.FC = () => {
-  const { isAuthenticated } = useAuthStore();
+  //const { isAuthenticated } = useAuthStore();
   /* if (!isAuthenticated) {
     return <NotPass />;
   } */
