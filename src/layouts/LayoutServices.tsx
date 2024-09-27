c4cc6ce64fc81c365c05afbe4b863d5eb91d1c59
Marton <martin.iribass@gmail.com>
2024-09-27 17:39:48 -0300

    "Added comments and code refactoring to BlogPostDetailMultipleRENDER and LayoutServices components"

--- src/layouts/LayoutServices.tsx
+++ src/layouts/LayoutServices.tsx
@@ -1,13 +1,16 @@
 import { BackgroundLines } from "@/components/ui/background-lines";
+import { useTheme } from "@/context/ThemeContext";
+import clsx from "clsx";
 import React from "react";
 import { Outlet } from "react-router-dom";
 
 const LayoutServices: React.FC = () => {
+  const { themeClasses } = useTheme();
   return (
     <>
       <div>
         <BackgroundLines className="flex items-center justify-center w-full flex-col px-4">
-          <h2 className="bg-clip-text text-transparent text-center bg-gradient-to-b from-neutral-900 to-neutral-700 dark:from-neutral-600 dark:to-white text-2xl md:text-4xl lg:text-7xl font-sans py-2 md:py-10 relative z-20 font-bold tracking-tight">
+          <h2 className={clsx(themeClasses.text,"bg-clip-text text-transparent text-center bg-gradient-to-b from-neutral-900 to-neutral-700 dark:from-neutral-600 dark:to-white text-2xl md:text-4xl lg:text-7xl font-sans py-2 md:py-10 relative z-20 font-bold tracking-tight")}>
             🚀 ¿Listo para transformar tu negocio?
           </h2>
           <p className="max-w-xl mx-auto text-md md:text-lg text-neutral-600 dark:text-neutral-400 text-center">
@@ -21,3 +24,7 @@ const LayoutServices: React.FC = () => {
 };
 
 export default LayoutServices;
+/* className={clsx(
+  "text-sm font-medium transition-colors hover:text-primary",
+  themeClasses.text
+)} */
