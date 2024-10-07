2ba24005e4afd63e72bae68624f5fc6fe68bbecb
Marton <martin.iribass@gmail.com>
2024-10-07 19:06:08 -0300

    "Added sourcemap option to build config in vite.config.ts"

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,10 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()], 
+  build: {
+    sourcemap: true,
+  },
   publicDir: "public",
   server: {
     open: true,
