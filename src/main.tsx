d7949b1130dc048b884cdc3ea678d605914a4881
Marton <martin.iribass@gmail.com>
2024-10-09 18:02:59 -0300

    "Update dependencies, refactor main.tsx, and modify tsconfig.json and vercel.json configurations"

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./global.css";
-import RoutesLayout from "./routes/RoutesLayout";
-import ErrorBoundary from "@/components/errorPage/ErrorBoundary";
-import { ThemeProvider } from "@/context/ThemeContext";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './global.css';
+import RoutesLayout from './routes/RoutesLayout';
+import ErrorBoundary from '@/components/errorPage/ErrorBoundary';
+import { ThemeProvider } from '@/context/ThemeContext';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <ErrorBoundary>
-      <ThemeProvider>
-        <RoutesLayout />
-      </ThemeProvider>
-    </ErrorBoundary>
-  </React.StrictMode>
-);
+const rootElement = document.getElementById('root');
+
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+
+  root.render(
+    <React.StrictMode>
+      <ErrorBoundary>
+        <ThemeProvider>
+          <RoutesLayout />
+        </ThemeProvider>
+      </ErrorBoundary>
+    </React.StrictMode>
+  );
+}
